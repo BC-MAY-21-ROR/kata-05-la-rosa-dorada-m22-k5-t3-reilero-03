36f2aa62270780ca08b941900edcc3e6b641f184
OwenLobato <owenlobatov@gmail.com>
2022-05-20 18:38:54 -0500

    Code refactor
    Co-authored-by Ever <ever864@gmail.com>

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,53 +1,43 @@
-class GildedRose
+# frozen_string_literal: true
 
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+  ITEMS = { N: 'Normal', AB: 'Ages Brie', BPTATC: 'Backstage passes to a TAFKAL80ETC concert',
+            SHOR: 'Sulfuras, Hand of Ragnaros', C: 'Conjured' }.freeze
+
+  def update_quality
+    @items.each do |x|
+      check_type(x)
+      x.sell_in -= 1 if x.name != [:SHOR]
+      check_quality(x)
+    end
+  end
+
+  def check_type(item)
+    if item.name != ITEMS[:AB] && item.name != ITEMS[:BPTATC]
+      item.quality -= 1 if item.quality.positive? && (item.name != ITEMS[:SHOR])
+    elsif item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == ITEMS[:BPTATC]
+        item.quality += 1 if item.sell_in <= 10 && (item.quality < 50)
+        item.quality += 1 if item.sell_in <= 5 && (item.quality < 50)
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
+    end
+  end
+
+  def check_quality(item)
+    if item.sell_in.negative?
+      if item.name != ITEMS[:AB]
+        if item.name != ITEMS[:BPTATC]
+          item.quality -= 1 if item.quality.positive? && (item.name != ITEMS[:SHOR])
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality -= item.quality
         end
+      elsif item.quality < 50
+        item.quality += 1
       end
     end
   end
@@ -62,7 +52,19 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
+
+i1 = Item.new('Normal', 0, 50)
+i2 = Item.new('Ages Brie', 2, 40)
+i3 = Item.new('Sulfuras, Hand of Ragnaros', 0, 50)
+i4 = Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 50)
+i5 = Item.new('Conjured', 0, 50)
+
+list_i = [i1, i2, i3, i4, i5]
+
+x = GildedRose.new(list_i)
+puts x.update_quality
+puts x.update_quality
